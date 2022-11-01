a296562618df5852fcecc9e1035fb6140bd030c7
Nestor Rubio <nestordrubio9@gmail.com>
2022-11-01 16:28:24 -0400

    added preliminary exercise schema

---
+++ backend/models/exercise.model.js
@@ -0,0 +1,60 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+var exerciseSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+    unique: false,
+    trim: true,
+    minlength: 2
+  },
+  description: {
+    type: String,
+    required: false,
+    unique: false,
+    trim: true
+  },
+  img: {
+    required: false,
+    unique: false,
+    data: Buffer,
+    contentType: String
+  },
+  exerciseType: {
+    type: String,
+    enum: ['AMRAP', 'CARDIO', 'SETSXREPS'],
+  },
+  exerciseInfo:{
+    sets:{
+      type: Number,
+      required: false,
+      unique: false
+    },
+    reps: {
+      type: Number,
+      required: false,
+      unique: false
+    },
+    time: {
+      type: Number,
+      required: false,
+      unique: false
+    },
+    weight: {
+      type: Number,
+      required: false,
+      unique: false
+    }
+  },
+  restTime:{
+    type: Number,
+    required: true,
+    unique: false
+  } 
+});
+
+const Exercise = mongoose.model('Exercise', exerciseSchema);
+
+module.exports = Exercise;

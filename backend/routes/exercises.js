5f9142806488322a60c99c21d09de5b86699459e
Nestor Rubio <nestordrubio9@gmail.com>
2022-11-05 14:48:12 -0400

    added delete, update, and individual get paths for exercises based on their object IDs provided in MongoDB

--- backend/routes/exercises.js
+++ backend/routes/exercises.js
@@ -1,12 +1,20 @@
 const router = require('express').Router();
 let Exercise = require('../models/exercise.model');
 
+//------GET-----//
 router.route('/').get((req,res) => {
   Exercise.find()
     .then(exercises => res.json(exercises))
     .catch(err => res.status(400).json('Error: ' + err));
 });
 
+router.route('/:id').get((req, res) => {
+  Exercise.findById(req.params.id)
+    .then(exercise => res.json(exercise))
+    .catch(err => res.status(400).json('Error: ' + err))
+})
+
+//-----POST-----//
 router.route('/add').post((req,res) => {
   // gather information to add exercise into database
   const title = req.body.title;
@@ -30,5 +38,31 @@ router.route('/add').post((req,res) => {
     .catch(err => res.status(400).json('Error: ' + err));
 
 });
+
+//------UPDATE-----//
+router.route('/:id').patch((req,res) => {
+  Exercise.findById(req.params.id)
+    .then(exercise => {
+      exercise.title = req.body.title;
+      exercise.description = req.body.description;
+      exercise.img = req.body.img;
+      exercise.exerciseType = req.body.exerciseType;
+      exercise.exerciseInfo = req.body.exerciseInfo;
+      exercise.restTime = req.body.restTime;
+
+      exercise.save()
+        .then(() => res.json(`Exercise ${exercise.title} updated`))
+        .catch(err => res.status(400).json('Error: ' + err));
+    })
+      .catch(err => res.status(400).json('Error: ' + err));
+});
+
+//------DELETE-----//
+router.route('/:id').delete((req,res) => {
+  Exercise.findByIdAndDelete(req.params.id)
+    .then(deletion => res.json(`Exercise ${deletion.title} deleted!`))
+    .catch(err => res.status(400).json('Error: ' + err));
+});
+
 
 module.exports = router;

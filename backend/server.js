647e5b85de37684eed08539ebed0185689d4397d
Nestor Rubio <nestordrubio9@gmail.com>
2022-11-07 13:57:04 -0500

    moved .env to root and added it to gitignore

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
-require('dotenv').config();
+require('dotenv').config({path: '../.env'});
 
 const app = express();
 const port = process.env.PORT || 5555;
